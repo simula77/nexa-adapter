da79f208b676f2fde7b4cf6b11d90ddca59b8572
Samuli Karjula <samuli.karjula@reaktor.fi>
2014-11-05 20:42:55 +0200

    Fix code broken by rebase and merge

--- adapter.js
+++ adapter.js
@@ -1,16 +1,16 @@
-var telldus = require('telldus');
-var _ = require('lodash');
-var ws = require('ws');
-var winston = require('winston');
+var telldus = require("telldus");
+var _ = require("lodash");
+var WebSocket = require("ws");
+var winston = require("winston");
 var logger = new (winston.Logger)({
     transports: [
       new (winston.transports.Console)(),
-      new (winston.transports.File)({ filename: 'logs/nexa-bridge.log', level: conf.logLevel })
+      new (winston.transports.File)({ filename: "nexa-adapter.log", level: "debug" })
     ]
   });
 var devices = telldus.getDevicesSync(function(err, devices) {
                 if ( err ) {
-                   logger.error('Failed to read devices from TellStick!', err);
+                   logger.error("Failed to read devices from TellStick!", err);
                    process.exit(1);
                  } else {
                    return JSON.stringify(devices);
@@ -18,16 +18,15 @@ var devices = telldus.getDevicesSync(function(err, devices) {
                });
 var houmioServer = process.env.HORSELIGHTS_SERVER || "wss://houm.herokuapp.com";
 var houmioSitekey = process.env.HORSELIGHTS_SITEKEY || "";
-var VENDOR = "nexa";
-var socket = new WebSocket(houmioServer);
+var ws = new WebSocket(houmioServer);
 
-ws.on('open', function() {
-  ws.send(JSON.stringify({ command: "publish", data: { sitekey: houmioSitekey, vendor: VENDOR } }));
-  logger.info('Connected to ' + houmioServer);
+ws.on("open", function() {
+  ws.send(JSON.stringify({ command: "publish", data: { sitekey: houmioSitekey, vendor: "nexa" } }));
+  logger.info("Connected to " + houmioServer);
 });
 
-ws.on('message', function(msg) {
-  logger.debug('received message %s', msg);
+ws.on("message", function(msg) {
+  logger.debug("received message %s", msg);
   handleMessage(JSON.parse(msg));
 })
 
@@ -37,14 +36,14 @@ function isDimmer(deviceId) {
   });
 }
 
-function parseDeviceId(address) {
-  return parseInt(address.substring(address.lastIndexOf("/") + 1, address.length));
+function parseOnOffValue(message) {
+  return message.data.on === true ? 1 : 0;
 }
 
-var handleMessage = function(message) {
-  var deviceId = parseDeviceId(message.data.groupaddress);
+function handleMessage(message) {
+  var deviceId = parseInt(message.data.devaddr);
   var dimmer = isDimmer(deviceId);
-  var value = dimmer ? parseInt(message.data.value, 16) : parseInt(message.data.value);
+  var value = dimmer ? parseInt(message.data.bri) : parseOnOffValue(message);
   if (dimmer && value > 0 && value < 255) {
     telldus.dimSync(deviceId, value);
   } else if (value === 0) {
@@ -52,19 +51,19 @@ var handleMessage = function(message) {
   } else {
     telldus.turnOnSync(deviceId);
   }
-};
+}
 
-ws.on('ping', function(ping) {
-  logger.debug('ping received');
+ws.on("ping", function(ping) {
+  logger.debug("ping received");
   ws.pong();
 });
 
-ws.on('error', function(error) {
-  logger.info('Error received: %s -> exiting', error);
+ws.on("error", function(error) {
+  logger.info("Error received: %s -> exiting", error);
   process.exit(1);
 });
 
-ws.on('close', function() {
-  logger.info('Connection closed -> exiting');
+ws.on("close", function() {
+  logger.info("Connection closed -> exiting");
   process.exit(1);
 });
